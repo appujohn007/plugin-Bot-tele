2bf06141fbbb6b3c7f278c0eee2fe7654c94bc34
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-07-02 10:36:36 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,5 @@
 from pyrogram import Client, filters
 
+@Client.on_message(filters.command("help") & filters.private)
 def help_command(client, message):
     message.reply_text('This is the help section.')
-
-def register(app: Client):
-    app.add_handler(filters.command("help") & filters.private, help_command)
