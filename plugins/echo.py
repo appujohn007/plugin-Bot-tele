c4c650a9063f324d9a7e8fe1d4d03b08f3681200
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-07-02 10:41:03 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
 
-@Client.on_message(filters.text & filters.private & ~filters.command)
+@Client.on_message(filters.text & filters.private)
 def echo(client, message):
     message.reply_text(message.text)
