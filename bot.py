1ea95a5ff00dd6ef71c8f45924dfca3659c51109
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-07-02 10:35:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,14 @@
-import os
-import importlib
 from pyrogram import Client
-
-# Load token and API credentials from config.py
 from config import API_ID, API_HASH, BOT_TOKEN
 
-# Function to dynamically load plugins
-def load_plugins(app):
-    plugins_folder = 'plugins'
-    for filename in os.listdir(plugins_folder):
-        if filename.endswith('.py') and filename != '__init__.py':
-            module_name = f"{plugins_folder}.{filename[:-3]}"
-            module = importlib.import_module(module_name)
-            if hasattr(module, 'register'):
-                module.register(app)
-
 def main():
-    app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-    # Load all plugins
-    load_plugins(app)
+    app = Client(
+        "my_bot",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        plugins=dict(root="plugins")
+    )
 
     app.run()
 
