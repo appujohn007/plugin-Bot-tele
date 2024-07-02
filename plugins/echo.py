0c0f99aa79eb60c72f2c5d23478e66e31530a7f0
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-07-02 10:36:55 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -1,7 +1,5 @@
 from pyrogram import Client, filters
 
+@Client.on_message(filters.text & filters.private & ~filters.command)
 def echo(client, message):
     message.reply_text(message.text)
-
-def register(app: Client):
-    app.add_handler(filters.text & filters.private & ~filters.command, echo)
